37fc57b10b7e0059d17c2db707d5845a33ef88e8
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-11-23 17:52:41 -0500

    Added stuff for the summary page

--- term_project/project/views.py
+++ term_project/project/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def summary(request):
 	return render(request, 'project/summary.html')
+	
+def installation(request):
+	return render(request, 'project/installation.html')
